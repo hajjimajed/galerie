a70ec3ce75564babe726ddd2d5cb39a023acdb11
hajjimajed <hajjimajed78@gmail.com>
2023-05-25 16:06:49 +0100

    adding update user data form and function

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -15,6 +15,7 @@ interface UserDataResponse {
 export class ProfileComponent implements OnInit {
 
   userData: any;
+  profileImage: File | undefined;
 
   constructor(
     private authService: AuthService,
@@ -29,7 +30,6 @@ export class ProfileComponent implements OnInit {
 
   getUserData() {
     const token = this.authService.getToken();
-    const userId = this.authService.getUserId();
 
     const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
@@ -43,9 +43,33 @@ export class ProfileComponent implements OnInit {
           console.log('error retrieving user data: ', error);
         }
       )
-
   }
 
+  onProfileImageChange(event: any) {
+    this.profileImage = event.target.files[0];
+  }
 
+  updateUserData() {
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+
+    const formData: FormData = new FormData();
+    formData.append('email', this.userData.email);
+    formData.append('name', this.userData.name);
+    if (this.profileImage) {
+      formData.append('image', this.profileImage, this.profileImage.name);
+    }
+
+    this.http.put<UserDataResponse>('http://localhost:8080/auth/update', formData, { headers })
+      .subscribe(
+        (response) => {
+          console.log('user data updated:', response);
+          this.userData = response.user;
+        },
+        (error) => {
+          console.log('error updating user data:', error);
+        }
+      );
+  }
 
 }
