08e8a1ad0af7324dac7b2f301e190cbc4df9adfa
hajjimajed <hajjimajed78@gmail.com>
2023-05-29 17:04:46 +0100

    adding styles to post in gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,27 +7,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-
   posts: any[] = [];
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchPosts();
   }
 
   fetchPosts() {
-    this.http.get('http://localhost:8080/post/posts')
-      .subscribe(
-        (response: any) => {
-          this.posts = response.posts;
-        },
-        (error) => {
-          console.error('error while retrieving posts', error);
-        }
-      )
+    this.http.get('http://localhost:8080/post/posts').subscribe(
+      (response: any) => {
+        this.posts = response.posts.map((post: any) => ({
+          ...post,
+          formattedDate: this.formatDate(post.date),
+        }));
+      },
+      (error) => {
+        console.error('Error while retrieving posts', error);
+      }
+    );
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    };
+    return date.toLocaleDateString('en-US', options);
   }
 
 }
